0783347ddabb7caeed8abaeef29a864414832dae
Carlos Perez <carlos.perez@charliegperez.com>
2020-06-30 00:49:07 -0500

    use of GraphQL and Apollo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+/* componente de orden superior, funcion que se le pasa un componente como parametro y devuelve otro componente ya sea con mejoras o con props inyectados */
+const withPhotos = graphql(gql`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8].map(id => <PhotoCard key={id} id={id} />)}
+      {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
